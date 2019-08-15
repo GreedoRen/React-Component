6eaa4eca4d37858bcf2015358374a6933bfd2171
GreedoRen <greedo.it@gmail.com>
2019-08-15 23:04:41 +0300

    React-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Navigation from './Navigation';
 import Main from './Main';
 import './App.css';
