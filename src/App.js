16f2c81eae2e06d9c9bf1eb66f5d5a5d35899f7f
GreedoRen <greedo.it@gmail.com>
2019-08-17 13:21:10 +0300

    Fix router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import Main from './components/Main/Main';
 import Page from './components/Page/Page';
 import Topic from './components/Topic/Topic';
-
+import Home from './components/Home/Home';
 import './App.css';
 
-function App() {
-	return (
-		<Router>
-			<Navigation />
-			<Route exact path='/' component={Home} />
-			<Route exact path='/main' component={Main} />
-			<Route exact path='/page' component={Page} />
-			<Route exact path='/topic' component={Topic} />
-		</Router>
-	);
+class App extends Component {
+	render() {
+		return (
+			<Switch>
+				<Navigation />
+				<Route exact path='/' component={Home} />
+				<Route exact path='/main' component={Main} />
+				<Route exact path='/page' component={Page} />
+				<Route exact path='/topic' component={Topic} />
+			</Switch>
+		);
+	}
 }
 
 export default App;
