0fc29e034115ff509f316975d06bccdef67d9c9e
GreedoRen <greedo.it@gmail.com>
2019-08-17 20:59:20 +0300

    Update css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ class Header extends Component {
 	render() {
 		return (
 			<header className='header'>
-				<span className='logo'>Logo</span>
+				<div className='logo'>Logo</div>
 				<input type='text' id='headerInput' name='neaderInput' />
 			</header>
 		);
