fa87e185772ce6f7fc1c0257cf5bd4a62e4aa282
GreedoRen <greedo.it@gmail.com>
2019-08-17 13:34:28 +0300

    Fix app and Navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import Main from './components/Main/Main';
 import Page from './components/Page/Page';
@@ -10,13 +10,17 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<Switch>
+			<Router>
 				<Navigation />
-				<Route exact path='/' component={Home} />
-				<Route exact path='/main' component={Main} />
-				<Route exact path='/page' component={Page} />
-				<Route exact path='/topic' component={Topic} />
-			</Switch>
+				<div className='App'>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/main' component={Main} />
+						<Route exact path='/page' component={Page} />
+						<Route exact path='/topic' component={Topic} />
+					</Switch>
+				</div>
+			</Router>
 		);
 	}
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './navigation.css';
 
 class Navigation extends Component {
 	render() {
 		return (
-			<Router>
-				<ul>
-					<li>
-						<Link to='/'>Home</Link>
-					</li>
-					<li>
-						<Link to='/main'>Main</Link>
-					</li>
-					<li>
-						<Link to='/page'>Page</Link>
-					</li>
-					<li>
-						<Link to='/topic'>Topic</Link>
-					</li>
-				</ul>
-			</Router>
+			<ul>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/main'>Main</Link>
+				</li>
+				<li>
+					<Link to='/page'>Page</Link>
+				</li>
+				<li>
+					<Link to='/topic'>Topic</Link>
+				</li>
+			</ul>
 		);
 	}
 }
